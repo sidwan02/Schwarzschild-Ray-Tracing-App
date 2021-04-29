ea96c9c99f4ea31b47bf74c12a7fd20805b22da7
Siddharth Diwan <sidwan02@gmail.com>
2021-04-29 11:29:43 -0400

    created + registered skelly position class

--- APISchwarzschildRayTracing/api/admin.py
+++ APISchwarzschildRayTracing/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Position
 
 # Register your models here.
+
+
+@admin.register(Position)
+class PositionModel(admin.ModelAdmin):
+    list_filter = ('x', 'y', 'z')
+    list_display = ('x', 'y', 'z')
--- APISchwarzschildRayTracing/api/models.py
+++ APISchwarzschildRayTracing/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Position(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
+
+    # def __str__(self):
+    #     return "(" + x + ", " + y + ", " + z + ")"
